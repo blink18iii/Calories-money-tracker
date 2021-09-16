60a846308a2666dc8d7605582b0b95e2170cad35
Max <blink18ii@gmail.com>
2021-09-16 08:39:15 +0200

    Начинаю разбираться

--- homework.py
+++ homework.py
@@ -1,9 +1,18 @@
 import datetime as dt
 
-
+date_format = '%d.%m.%Y'
+    def __init__(self, limit: float) -> None:
+        self.limit = limit
+        self.records = []        
 class Calculator:
     pass
 
 
 class Record:
+    def __init__(self,amount: float, date: int, comment: str = None):
+        self.amount = float(amount)
+        self.date = date
+        self.comment = comment
+
+    
     pass

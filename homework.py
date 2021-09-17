bbe4626f908f2c21972430d446209b8caea1bfea
Max <blink18ii@gmail.com>
2021-09-17 15:48:52 +0200

    пора проверять

--- homework.py
+++ homework.py
@@ -4,46 +4,31 @@ date_format = '%d.%m.%Y'
 
 
 class Calculator:
-    def __init__(self, limit: float) -> None:
+    def __init__(self, limit: float):
         self.limit = limit
         self.records = []
 
-    def add_record(self,new_record: str) -> None:
-        """Adding new record"""
-        self.records.append(new_record)
+    def add_record(self):
+        date_today = dt.date.today()
+        pass
 
-    def get_today_stats():
-        """How much calories were eaten"""
-        today_stats = dt.date.today()
-        sum(record.amount)
+    def get_today_stats(self):
+        pass
 
-    def get_calories_remained():
-
-
-    def get_week_stats():
-        """Record total week calories"""
-        today_stats = dt.date.today()
-        week_ago_stats = today_stats - dt.timedelta(days = 7)
-        return float(sum(record.amount))
-
-
-class CashCalculator(Calculator):
-    USD_RATE = 80.0
-    EURO_RATE = 90.0
-    RUB_RATE = 1.0
-
-    def get_today_cash_remained(self, currency: float):
-
+    def get_week_stats(self):
+        pass
 
 
 class Record:
-    """Creating a record"""
     def __init__(self, amount: float, comment: str, date: str = None):
-        self.amount = float(amount)
-        self.comment = comment
-        self.date = date
-        if date is not None:
-            self.date = dt.datetime.strptime(date, date_format).date()
-        else:
-            self.date = dt.datetime.today()
+        pass
 
+
+class CashCalculator(Calculator):
+    def get_today_cash_remaining(self, currency: str):
+        pass
+
+
+class CaloriesCalculator(Calculator):
+    def get_calories_remained(self):
+        pass
